227ccf4428800b2d4897e63b9af3524801d3602a
Thomas Hladish <tjhladish@ufl.edu>
2019-05-03 15:14:09 -0400

    Refactored MosquitoModel.py

--- MosquitoModel.py
+++ MosquitoModel.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import random
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,91 +7,76 @@ import networkx as nx
 import math
 
 class MosquitoSim:
-  #Constructor
-  def __init__(self, days, rate, pervar, initlocationList, randomness, netgraph):
-    if len(initlocationList) != len(netgraph):
-      raise Exception('Error: the number of notes and initial locations don\'t match')
-      
-    self.days = days
-    self.rate = rate
-    self.pervar = pervar
-    self.locationList = initlocationList
-    self.randomness = randomness
-    self.netgraph = netgraph
-    
-    self.xs = [0]
-    self.ys = [0]
-    self.shouldround = False
-	
+    #Constructor
+    def __init__(self, days, rate, pervar, initlocationList, randomness, netgraph):
+        if len(initlocationList) != len(netgraph):
+            raise Exception('Error: the number of notes and initial locations don\'t match')
+
+        self.days = days
+        self.rate = rate
+        self.pervar = pervar
+        self.locationList = initlocationList
+        self.randomness = randomness
+        self.netgraph = netgraph
+
+        self.xs = []
+        self.ys = []
+        self.shouldround = False
+
 #Helper Methods
-  def rounddecimal (self,number):
-    if int(number) == number-0.5: return int(number) + 1
-    else: return round(number)
+    def randomrate (self,num_mos,move_prob):
+        return np.random.binomial(num_mos, move_prob)
 
-  def randomrate (self,value):
-    numMosquitos = np.random.normal(value, (math.sqrt(value*0.25)*self.pervar))
-    if numMosquitos < 0:
-      return 0
-    elif numMosquitos > 2*value:
-      return 2*value
-    else:
-      return numMosquitos
-    '''total = 0
-    for b in range (0, 2*int(value)):
-      c = random.randint(0,1)
-      total += c
-    return (total)'''
-
-  def distrmos(self,loc0,inputlist):
-    if self.randomness == "ideal":
-      mosmoving = self.locationList[loc0]*self.rate*self.pervar
-    else:
-      mosmoving = self.randomrate(self.locationList[loc0]*self.rate)
-    self.locationList[loc0] -= mosmoving
-    edlist = list(self.netgraph.adj[loc0])
-    if len(edlist) > 0:
+    def distrmos(self,loc_idx,changemoslocs):
+        num_mos = self.locationList[loc_idx]
         if self.randomness == "ideal":
-          for ed in edlist:
-            inputlist[ed] += mosmoving/(len(edlist))
+            mosmoving = num_mos*self.rate*self.pervar
         else:
-          for mos in range (0,mosmoving):
-            rand = random.randint(0,len(edlist)-1)
-            d = edlist[rand]
-            inputlist[d] +=1
+            mosmoving = np.random.binomial(num_mos, self.rate)
 
-  def simmos(self):
+        self.locationList[loc_idx] -= mosmoving
 
-    for loc1 in range (0,len(self.locationList)): self.ys.append ([self.locationList[loc1]])
+        edlist = list(self.netgraph.adj[loc_idx])
+        num_neighbors = len(edlist)
+        if num_neighbors > 0:
+            if self.randomness == "ideal":
+                for ed in edlist:
+                    changemoslocs[ed] += mosmoving/num_neighbors
+            else:
+                counts = np.random.multinomial(mosmoving, [1.0/num_neighbors]*num_neighbors)
+                for i in range(num_neighbors):
+                    dest = edlist[i]
+                    changemoslocs[dest] += counts[i]
 
-    for day in range (1,self.days+1):
-      self.xs.append (day)
-      while True:
-        changemoslocs= [0] * len(self.locationList)
-        totalmos = 0
-        for loc2 in range (0,len(self.locationList)): totalmos += self.locationList[loc2]
-        for loc3 in range (0,len(self.locationList)): self.distrmos(loc3,changemoslocs)
-        for loc4 in range (0,len(self.locationList)-1): self.locationList[loc4]+=changemoslocs[loc4]
-        self.locationList[len(self.locationList)-1] = totalmos
-        for loc5 in range (0,len(self.locationList)-1): self.locationList[len(self.locationList)-1] -= self.locationList[loc5]
-        for loc6 in range (0,len(self.locationList)): 
-          if self.locationList[loc6] < 0: continue
-        for loc7 in range (1,len(self.locationList)+1): 
-          self.ys[loc7].append (self.locationList[(loc7-1)])
-        break
+    def simmos(self):
+        loc_idxs = range(len(self.locationList))
+        self.xs.append(0)
+        for i in loc_idxs:
+            self.ys.append([self.locationList[i]])
 
-  def graph(self):
-    print ("Days:", self.days)
-    print ("Rate:", self.rate)
-    for loc8 in range (1,len(self.locationList)+1): plt.plot(np.array(self.xs),np.array(self.ys[loc8]),label = (loc8-1))
-    plt.xlabel('Days') 
-    plt.ylabel('Number of mosquitos') 
-    plt.title('Population of Mosquitos Over Time') 
-    plt.legend()
-    plt.show()
-    
-  def simulateAndGraph(self):
-    self.simmos()
-    self.graph()
+        for day in range(self.days):
+            self.xs.append(day+1)
+            changemoslocs = [0] * len(self.locationList)
+            for i in loc_idxs:
+                self.distrmos(i,changemoslocs)
+            for i in loc_idxs:
+                self.locationList[i]+=changemoslocs[i]
+                self.ys[i].append(self.locationList[i])
+
+
+    def graph(self):
+        print ("Days:", self.days)
+        print ("Rate:", self.rate)
+        for i in range(len(self.locationList)): plt.plot(np.array(self.xs),np.array(self.ys[i]),label = i)
+        plt.xlabel('Days')
+        plt.ylabel('Number of mosquitos')
+        plt.title('Population of Mosquitos Over Time')
+        plt.legend()
+        plt.show()
+
+    def simulateAndGraph(self):
+        self.simmos()
+        self.graph()
 
 g=nx.Graph()
 g.add_edges_from([(0,1),(1,2),(2,3),(3,0)])
@@ -106,10 +93,9 @@ sims = [MosquitoSim(150,i/100,1,[10000,0,0,0],'random') for i in range (20,21)] 
   #sim.simmos()
   #sim.graph()
 '''
-  
- 
-sim = MosquitoSim(150,4/100,1,[1000,0,5,0],'random', g)
-sim1 = MosquitoSim(150,4/100,1,[500,0],'ideal', h)
+
+sim = MosquitoSim(150, 0.04, 1, [1000,0,5,0], 'random', g)
+sim1 = MosquitoSim(150, 0.04, 1, [500,0], 'ideal', h)
 #sim1.shouldround = True
 
 sim.simulateAndGraph()
