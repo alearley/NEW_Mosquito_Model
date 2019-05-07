58b3e53ef1ae0c6e6ecd024d0ad68daa578ae3ed
alearley <47512638+alearley@users.noreply.github.com>
2019-05-07 11:06:02 -0400

    Add files via upload
    Took out unnecessary code and replaced the terms "random" and  "ideal" with "stochastic" and "deterministic"

--- MosquitoModel.py
+++ MosquitoModel.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 
-import random
 import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
-import math
 
 class MosquitoSim:
     #Constructor
-    def __init__(self, days, rate, pervar, initlocationList, randomness, netgraph):
+    def __init__(self, days, rate, var, initlocationList, randomness, netgraph):
         if len(initlocationList) != len(netgraph):
             raise Exception('Error: the number of notes and initial locations don\'t match')
 
         self.days = days
         self.rate = rate
-        self.pervar = pervar
+        self.var = var
         self.locationList = initlocationList
         self.randomness = randomness
         self.netgraph = netgraph
 
         self.xs = []
         self.ys = []
-        self.shouldround = False
 
 #Helper Methods
     def randomrate (self,num_mos,move_prob):
@@ -29,24 +26,27 @@ class MosquitoSim:
 
     def distrmos(self,loc_idx,changemoslocs):
         num_mos = self.locationList[loc_idx]
-        if self.randomness == "ideal":
-            mosmoving = num_mos*self.rate*self.pervar
-        else:
+        if self.randomness == "deterministic":
+            mosmoving = num_mos*self.rate*self.var
+        elif self.randomness == "stochastic":
             mosmoving = np.random.binomial(num_mos, self.rate)
+        else: raise Exception('Error: Please input deterministic or stochastic')
+            
 
         self.locationList[loc_idx] -= mosmoving
 
         edlist = list(self.netgraph.adj[loc_idx])
         num_neighbors = len(edlist)
         if num_neighbors > 0:
-            if self.randomness == "ideal":
+            if self.randomness == "deterministic":
                 for ed in edlist:
                     changemoslocs[ed] += mosmoving/num_neighbors
-            else:
+            elif self.randomness == "stochastic":
                 counts = np.random.multinomial(mosmoving, [1.0/num_neighbors]*num_neighbors)
                 for i in range(num_neighbors):
                     dest = edlist[i]
                     changemoslocs[dest] += counts[i]
+            else: raise Exception('Error: Please input deterministic or stochastic')
 
     def simmos(self):
         loc_idxs = range(len(self.locationList))
@@ -83,20 +83,19 @@ g.add_edges_from([(0,1),(1,2),(2,3),(3,0)])
 #g.add_node(0)
 
 h = nx.Graph()
-h.add_edges_from([(0,1),(1,0)])
+h.add_edges_from([(0,1),(1,2),(2,0)])
 #print (nx.info(g))
 #nx.draw(g, with_labels = True)
 
 '''
-sims = [MosquitoSim(150,i/100,1,[10000,0,0,0],'random') for i in range (20,21)] #last input should be 'random' or 'ideal'
+sims = [MosquitoSim(150,i/100,1,[10000,0,0,0],'stochastic') for i in range (20,21)] #last input should be 'stochastic' or 'deterministic'
 for sim in sims: 
   sim.simmos()
   sim.graph()
 '''
 
-sim = MosquitoSim(150, 0.04, 1, [1000,0,5,0], 'random', g)
-sim1 = MosquitoSim(150, 0.04, 1, [500,0], 'ideal', h)
-#sim1.shouldround = True
+sim = MosquitoSim(10, 0.05, 1, [30,23,45,23], 'stochastic', g)
+sim1 = MosquitoSim(25, 0.04, 1, [234,574,326], 'deterministic', h)
 
 sim.simulateAndGraph()
 sim1.simulateAndGraph()
